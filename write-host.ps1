f7657466dfa2c5ab9717eff7aa36f54a9c6e122b
Knarute <kiran.narute91@gmail.com>
2022-05-01 12:12:58 +0530

    Fisrt Commit

---
+++ write-host.ps1
@@ -0,0 +1,14 @@
+﻿function Write-NAme ($Name,$Surname)
+{
+    Write-Host "Welocme $Name $Surname"
+}
+
+function Get-Name ()
+{
+  $Name=Read-Host "Entern Your First Name"
+  $Surname=Read-host "Enter your Surname"  
+return $Name , $Surname
+}
+
+$K=Get-Name
+Write-NAme $K
